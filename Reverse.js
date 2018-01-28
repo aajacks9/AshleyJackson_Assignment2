2072ad14e12615aa2f7ce4a87030f1e187fec0c5
aajacks9 <aajacks@gmail.com>
2018-01-28 17:35:06 -0600

    comment of steps
    I typed out each step that needs to happen

--- Reverse.js
+++ Reverse.js
@@ -1,5 +1,12 @@
 
+/* input string
+get the length of the string
+split the string into individual strings each having a position
+order the string positions from last to first
+rejoin into one string again
+output new reverse string
 
+ */
 
 
 var txt = 'Ashley'
