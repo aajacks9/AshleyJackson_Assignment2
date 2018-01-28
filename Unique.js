7ab4a1935d962276d3433db24d13e2bd8387e017
aajacks9 <aajacks@gmail.com>
2018-01-28 17:39:47 -0600

    Outlining Unique function

---
+++ Unique.js
@@ -0,0 +1,19 @@
+/*
+input the array
+determine how many strings are in the array
+compare each string to every other string
+if two are the same return false
+if all are different return true
+ */
+
+
+
+IsUnique(['happy, sad, grumpy, happy']){
+    if ( all are unique
+)
+        return true;
+    if else
+
+        return false;
+
+}
